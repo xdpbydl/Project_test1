c11a360be50bf638e5531f97ca753b4a4f04fbb3
Administrator <q1>
2019-08-24 20:38:43 +0800

    test

---
+++ 03.批量改名_X1.py
@@ -0,0 +1,29 @@
+﻿# -*- coding: utf-8 -*-
+'''批量改名'''
+import shutil, os
+from openpyxl import load_workbook
+
+srcDir = 'D:/Temp/orher/111/'
+targetDir = 'D:/Temp/orher/222/'
+file_list = sorted(os.listdir(srcDir))
+
+file_list_iter = iter(file_list)
+
+# for index1, file_name in enumerate(file_list):
+#     for name in file_name:
+#         print(name, end="")
+#     print("")
+#
+wb = load_workbook(r"D:/Temp/orher/26个班级.xlsx", data_only=True)
+sh = wb["Sheet1"]
+
+for index, item in enumerate(sh["I2":"I51"]):
+    for cell in item:
+        print(cell)
+        name = cell.value
+        # print(name)
+        srcfile = srcDir + next(file_list_iter)
+        dstfile = targetDir + name + ".jpg"
+        print(srcfile , "》》》》》", name + ".jpg")
+
+        shutil.copyfile(srcfile, dstfile)
