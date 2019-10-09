71b0de07bf7bdcbb4c81bf4bd69f39eba0ae3a8f
xdpbydl <tzhahu@163.com>
2019-10-09 18:13:31 +0800

    EE

--- test.py
+++ test.py
@@ -1,45 +1,46 @@
-# -*- coding: utf-8 -*-
+import random
+import requests
+import re
 
-# @File    : switch_tab.py
-# @Date    : 2018-07-27
-# @Author  : Peng Shiyu
+'''
+利用访问http://icanhazip.com/返回的IP进行测试
+说明：利用的http://icanhazip.com/返回的IP进行校验，如返回的是代理池的IP，说明代理有效，否则实际代理无效
+'''
 
-import time
-from selenium import webdriver
+# 代理ip池
+PROXIES_NEW = {}
+
+lens = len(PROXIES_NEW['https'])
+print(lens)
+num = 1
+while num <= lens:
 
 
-browser = webdriver.Chrome()
+    try:
+        requests.adapters.DEFAULT_RETRIES = 3
+        proxies = PROXIES_NEW['https']
 
-# 在当前浏览器中访问百度
-browser.get('https://www.baidu.com')
-
-# 新开一个窗口，通过执行js来新开一个窗口
-js = 'window.open("https://www.sogou.com");'
-browser.execute_script(js)
-
-# 输出当前窗口句柄（百度）
-baidu_handle = browser.current_window_handle
-
-# 获取当前窗口句柄集合（列表类型）
-handles = browser.window_handles
-print(handles)  # 输出句柄集合
-# ['CDwindow-E9B85270B51D42AF7369D81B9AA70FFE',
-# 'CDwindow-90004FD79A0F59EE057846B34D0E7327']
-
-# 获取搜狗窗口
-sogou_handle = None
-for handle in handles:
-    if handle != baidu_handle:
-        sogou_handle = handle
-
-# 输出当前窗口句柄（搜狗）
-print('switch to ', handle)
-browser.switch_to.window(sogou_handle)
-time.sleep(10)
-browser.close() #关闭当前窗口（搜狗）
-
-# 切换回百度窗口
-browser.switch_to.window(baidu_handle)
-
-time.sleep(10)
-browser.quit()
+        IP = random.choice(proxies)
+        # print(IP)
+        # print(type(IP))
+        b = re.findall('//(\d+\.\d+\.\d+\.\d+):', IP)[0]
+        b = b.replace('.', '')
+        print(b)
+        # thiProxy = "http://" + IP
+        # thisIP = "".join(IP.split(":")[0:1])
+        # print(thisIP)
+        res = requests.get(url="http://icanhazip.com/", timeout=8, proxies={"https": "https://113.140.1.82:53281"})
+        proxyIP = res.text
+        # print(proxyIP)
+        a = proxyIP.replace('.', '')
+        print(a)
+        if int(a) == int(b):
+            print("代理IP:'" + proxyIP + "'有效！")
+        else:
+            print("返回不是代理池中的ip，代理IP无效！")
+    except:
+        print("代理IP无效！")
+        print(111)
+    # else:
+    #     print('success')
+    num += 1
