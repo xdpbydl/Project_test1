8e627f5f1dd37412b3e58f9165ba73d59549cd88
xdpbydl <tzhahu@163.com>
2019-10-31 17:52:24 +0800

    zcxx

--- test11.py
+++ test11.py
@@ -11,25 +11,40 @@ import time as t
 # options.headless = True
 # driver = webdriver.Firefox (options=options)
 driver = webdriver.Firefox ()
-driver.implicitly_wait (30)
-driver.get ("http://localhost/grwl/Login.aspx/")
+# driver = webdriver.Ie()
+driver.implicitly_wait (20)
+driver.get ("http://127.0.0.1/grwl/Login.aspx")
+driver.maximize_window ()
+
 
 # 输入
 txtUser = driver.find_element_by_xpath ('//*[@id="txtUser"]')
 txtUser.clear()
-txtUser.send_keys ('admin')
+txtUser.send_keys ('ZCAdmin')
 
 # 使用JS来输入,对于 密码可能存在问题
 # js_value = 'document.getElementById("txtPwd1").value="1"'
 # driver.execute_script(js_value)
 
+driver.switch_to.active_element.send_keys(Keys.TAB)
+time.sleep(0.3)
+driver.switch_to.active_element.send_keys('1')
+time.sleep(0.3)
 
 
-js = 'document.getElementById("txtPwd1").removeAttribute("readonly");'
-driver.execute_script(js)
-t.sleep(3)
-pwd = driver.find_element_by_xpath ('//*[@id="txtPwd1"]')
-pwd.clear()
-pwd.send_keys('1')
-# pwd.send_keys (Keys.RETURN)
+# js = 'document.getElementById("txtPwd1").removeAttribute("readonly");'
+# driver.execute_script(js)
+# t.sleep(3)
+# pwd = driver.find_element_by_xpath ('//*[@id="txtPwd1"]')
+# pwd.clear()
+# pwd.send_keys('aaaa')
+# # pwd.send_keys (Keys.RETURN)
 driver.find_element_by_xpath('//*[@id="imgbtnLogin"]').click()
+
+t.sleep(3)
+cookies = driver.get_cookies ()
+# print(cookies) #ie下存在问题
+driver.find_element_by_css_selector('#TreeView1n1 > img:nth-child(1)').click()
+driver.find_element_by_css_selector('#TreeView1t3').click()     #合同发起
+driver.find_element_by_css_selector('#btnAdd').click()      #添加
+print("#"*15)
--- test12.py
+++ test12.py
@@ -1,9 +1,29 @@
-from selenium import webdriver
+# coding=utf-8
 
-browser = webdriver.Chrome ()
-browser.maximize_window ()
-username = "user"  # stands for !@user
-password = "Password"  # stands for ^&pass
-url = "localhost/grwl/Login.aspx"
-webUrl = 'http://{}:{}@{}'.format (username, password, url)
-browser.get (webUrl)
+from selenium import webdriver
+import time
+import json
+
+
+def get_cookies(test_url):
+    # 保存cookies的文件
+    file = 'cookies.json'
+    # 打开需要获取cookies的网站
+    driver = webdriver.Ie ()
+    driver.implicitly_wait (5)
+    driver.get (test_url)
+    driver.maximize_window ()
+    # 网站打开后，在时间内手动执行登录操作
+    time.sleep (60)
+    # 登录成功后，获取cookies并保存为json格式
+    cookies = driver.get_cookies ()
+    fp = open (file, 'w')
+    json.dump (cookies, fp)
+    fp.close ()
+    # 关闭浏览器
+    driver.close ()
+
+
+if __name__ == "__main__":
+    url = 'http://127.0.0.1/grwl/Login.aspx/'
+    get_cookies (url)
