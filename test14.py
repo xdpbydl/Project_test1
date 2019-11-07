956c7916da8cdc9f6c98a8730543e4b75ce02692
xdpbydl <tzhahu@163.com>
2019-11-07 18:02:20 +0800

    zcxx

--- test14.py
+++ test14.py
@@ -1,6 +1,25 @@
-import socket,socks,requests
+# coding:utf-8
+from selenium import webdriver
+import time
+driver = webdriver.Firefox()
+driver.get("http://www.cnblogs.com/yoyoketang/")
 
-socks.set_default_proxy(socks.SOCKS5,"127.0.0.1",9150)
-socket.socket = socks.socksocket
-a = requests.get ("http://checkip.amazonaws.com").text
-print(a)
+# 定位首页管理按钮：id=blog_nav_contact
+js1 = 'document.getElementById("blog_nav_contact").click();'
+driver.execute_script(js1)
+
+# 输入账号
+js2 = 'document.getElementsByClassName("form-control")[0].value="上海-悠悠";'
+driver.execute_script(js2)
+
+# 输入密码
+js3 = 'document.getElementsByClassName("form-control")[1].value="xxx";'
+driver.execute_script(js3)
+
+# 勾选记住密码
+js4 = 'document.getElementsByName("custom-control-label")[0].click();'
+driver.execute_script(js4)
+
+# 点击登录按钮
+js5 = 'document.querySelectorAll("#signin")[0].click();'
+driver.execute_script(js5)
